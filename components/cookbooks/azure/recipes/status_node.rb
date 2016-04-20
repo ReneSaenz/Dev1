ff0c01e75d2f5352ddfc01aff9325bc7c1544c63
Rene Saenz <edgar.saenz@walmart.com>
2016-04-20 09:46:05 -0500

    testing jenkins pipeline

--- components/cookbooks/azure/recipes/status_node.rb
+++ components/cookbooks/azure/recipes/status_node.rb
@@ -11,6 +11,7 @@ AzureCommon::AzureUtils.set_proxy_from_env(node)
 include_recipe 'azure::get_platform_rg_and_as'
 include_recipe "azure::get_credentials"
 Chef::Log.info("Starting status ...")
+Chef::Log.info("Starting status ...")
 begin
       client = ComputeManagementClient.new(node['azureCredentials'])
       client.subscription_id = node['subscriptionid']
