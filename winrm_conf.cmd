dc91d6f34c20e0ddc77db9219200c7d89c4a16cb
Rene Saenz <edgar.saenz@walmart.com>
2016-05-29 19:18:46 -0500

    WinRM configuration

---
+++ winrm_conf.cmd
@@ -0,0 +1,11 @@
+
+call winrm quickconfig
+call winrm set winrm/config/client @{AllowUnencrypted="true"}
+call winrm set winrm/config/client/auth @{Basic="true"}
+call winrm set winrm/config/client @{TrustedHosts="*"}
+
+call winrm create winrm/config/listener?Address=*Transport=HTTP"
+call winrm set winrm/config/service @{AllowUnencrypted="true"}
+call winrm set winrm/config/service/auth @{Basic="true"}
+call winrm set winrm/config/service/auth @{Certificate="true"}
+
