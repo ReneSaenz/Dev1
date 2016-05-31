6841b93ae57e61abeff6f838e2c201d11b803ffd
Rene Saenz <edgar.saenz@walmart.com>
2016-05-31 14:11:53 -0500

    fixed typos

--- winrm_conf.cmd
+++ winrm_conf.cmd
@@ -4,8 +4,7 @@ call winrm set winrm/config/client @{AllowUnencrypted="true"}
 call winrm set winrm/config/client/auth @{Basic="true"}
 call winrm set winrm/config/client @{TrustedHosts="*"}
 
-call winrm create winrm/config/listener?Address=*+Transport=HTTP"
+call winrm create winrm/config/listener?Address=*+Transport=HTTP
 call winrm set winrm/config/service @{AllowUnencrypted="true"}
 call winrm set winrm/config/service/auth @{Basic="true"}
-call winrm set winrm/config/service/auth @{Certificate="true"}
 
